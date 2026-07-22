d0da8e12f5ce7cdcad0e7031fd58fa52f63bc4cc
BilalAS173 <bilalahmedshariff2005@gmail.com>
2026-07-22 17:27:13 +0500

    Added some redux work

---
+++ frontend/src/store/projectSlice.js
@@ -0,0 +1,16 @@
+import createSlice from "@reduxjs/toolkit";
+const projectSlice= createSlice({
+    name: "project",
+    initialState: null,
+    renders:  {
+        selectProject(state, action) {
+            return action.payload;
+        },
+        clearProject () {
+            return null;
+        },
+    },
+});
+
+export const {selectProject, clearProject}=projectSlice.actions;
+export default projectSlice.reducer;
