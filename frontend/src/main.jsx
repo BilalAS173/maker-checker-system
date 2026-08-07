d4d39ac204d795fa8e22bc9bf008adf365bc02dd
BilalAS173 <bilalahmedshariff2005@gmail.com>
2026-08-07 17:14:15 +0500

    Minor change in the main.jsx file.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react'
 import {BrowserRouter} from "react-router-dom"
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.tsx'
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
 import { store } from './store/store'
 import { Provider } from "react-redux";
